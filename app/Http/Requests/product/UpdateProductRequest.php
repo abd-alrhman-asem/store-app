8c6abfd16d14855f0f27f4fc3f6b9fb3945b653c
abd alrhman <abd.alrhman.asem.bashir@gmail.com>
2024-06-05 12:28:59 +0300

    fet(product user story)

---
+++ app/Http/Requests/product/UpdateProductRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\product;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+//            'category_id' => 'sometimes|required|exists:categories,id',
+        ];
+    }
+    protected function failedValidation(Validator $validator): void
+    {
+        $errorMessages = $validator->errors()->all();
+        throw new HttpResponseException(
+            response: unprocessableResponse(
+                $errorMessages
+            )
+        );
+    }
+}
